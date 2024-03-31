cb832f4bbfde632c62d0a354f8298c5dea907373
Chinmay <chinmaygope69@gmail.com>
2024-03-31 22:10:19 +0530

    Threads app

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,24 +1,44 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+
+import Topbar from "@/components/shared/Topbar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
 import "../globals.css";
+import { dark } from "@clerk/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Threads App",
-  description: "A Threads clone in Nextjs",
+export const metadata = {
+  title: "Threads",
+  description: "A Nextjs13 Meta Threads Application",
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <html lang='en'>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Topbar />
+          <main className='flex'>
+            <LeftSidebar />
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,13 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-// See https://clerk.com/docs/references/nextjs/auth-middleware
-// for more information about configuring your Middleware
 export default authMiddleware({
-  // Allow signed out users to access the specified routes:
-  publicRoutes: ["/", "/api/webhook/clerk"],
+  // An array of public routes that don't require authentication.
+  publicRoutes: ["/api/webhook/clerk"],
+
+  // An array of routes to be ignored by the authentication middleware.
   ignoredRoutes: ["/api/webhook/clerk"],
 });
 
 export const config = {
-  matcher: [
-    // Exclude files with a "." followed by an extension, which are typically static files.
-    // Exclude files in the _next directory, which are Next.js internals.
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    // Re-include any files in the api or trpc folders that might have an extension
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
